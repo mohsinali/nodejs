de88bfe870c3737b5fa7ef9456b3a83459e6bde8
Mohsin Ali <mohsin@mohsin-HP-ProBook.(none)>
2014-08-29 10:14:57 +0500

    First commit.

---
+++ my_parser.js
@@ -0,0 +1,25 @@
+// Load the fs(file system) module
+var fs = require('fs');
+
+fs.readFile('example_log.txt', function(err, logData){
+  if (err) throw err; 
+
+  var text = logData.toString();
+
+  var results = {};
+
+  var lines = text.split('\n');
+
+  lines.forEach(function(line){
+    var parts = line.split(' ');
+    var letter = parts[1];
+    var count = parts[2];
+
+    if(!results[letter])
+      results[letter] = 0
+
+    results[letter] += parseInt(count);
+  });
+
+  console.log(results);
+});
